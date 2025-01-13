563a2d1a6966bb8993d4f9b339f3a25f28928bcc
Chestnut <88202804+qqingzheng@users.noreply.github.com>
2025-01-13 20:57:54 +0800

    update

--- genstyle_common/micro_service/serve.py
+++ genstyle_common/micro_service/serve.py
@@ -6,7 +6,7 @@ from ..logger.logger import log
 
 async def register_service(handlers: list[BaseHandler]):
     """注册服务"""
-    
+    print("服务注册中", flush=True)
     # 连接到 RabbitMQ
     await log(type="info", message=f"服务注册中")
     connection = await get_rabbitmq_connection()
@@ -15,6 +15,7 @@ async def register_service(handlers: list[BaseHandler]):
         exchange = channel.default_exchange
         # 初始化处理器
         for handler in handlers:
+            print(f"注册处理器: {handler}", flush=True)
             handler_instance = handler(exchange)
             queue = await channel.declare_queue(handler.hanlder_name)
             # 注册消费者
@@ -25,6 +26,7 @@ async def register_service(handlers: list[BaseHandler]):
                     handler_func=handler
                 )
             )
+            print(f"服务注册完成 {handler}", flush=True)
         await log(type="info", message=f"服务注册完成 {str(handlers)}")
         try:
             await asyncio.Future()  # 持续运行
