a5b98cbdda3e9eb2f5869827f81fa91bbef750d3
Chestnut <88202804+qqingzheng@users.noreply.github.com>
2025-01-13 20:56:16 +0800

    update

--- genstyle_common/micro_service/handler.py
+++ genstyle_common/micro_service/handler.py
@@ -9,16 +9,16 @@ from ..schemas import ErrorResponse
 
 class BaseHandler:
     """基础消息处理器，提供通用逻辑"""
-
+    request_model: Type[BaseModel]
+    response_model: Type[BaseModel]
+    hanlder_name: str
+    
     def __init__(self, exchange: aio_pika.Exchange):
         self.exchange = exchange
 
     async def process_message(
         self,
         message: aio_pika.IncomingMessage,
-        request_model: Type[BaseModel],
-        response_model: Type[BaseModel],
-        handler_func,
     ):
         """处理消息的通用逻辑"""
         async with message.process():
@@ -27,7 +27,7 @@ class BaseHandler:
                 body = json.loads(message.body.decode())
                 # 校验 request_data
                 try:
-                    request_data = request_model(**body)
+                    request_data = self.request_model(**body)
                 except ValidationError as e:
                     response = ErrorResponse(
                         message=f"Request data validation failed: {e}"
@@ -35,7 +35,7 @@ class BaseHandler:
                     return response
 
                 # 调用具体处理函数
-                response = await handler_func(request_data)
+                response = await self.__call__(request_data)
             except Exception as e:
                 response = ErrorResponse(message=f"{str(e)}")
 
@@ -43,19 +43,9 @@ class BaseHandler:
             if message.reply_to:
                 await self.exchange.publish(
                     aio_pika.Message(
-                        body=json.dumps(response.dict()).encode(),
+                        body=json.dumps(response.model_dump()).encode(),
                         correlation_id=message.correlation_id,
                     ),
                     routing_key=message.reply_to,
                 )
 
-def handler(queue_name: str, request_model: Type[BaseModel], response_model: Type[BaseModel]):
-    """处理器装饰器，用于自动注册处理函数"""
-
-    def decorator(func):
-        func._queue_name = f"{os.getenv('SERVICE_NAME')}_{queue_name}"
-        func._request_model = request_model
-        func._response_model = response_model
-        return func
-
-    return decorator
--- genstyle_common/micro_service/serve.py
+++ genstyle_common/micro_service/serve.py
@@ -4,46 +4,28 @@ from .handler import BaseHandler
 from ..conn.rabbitmq import get_rabbitmq_connection
 from ..logger.logger import log
 
-async def register_service(handler: BaseHandler):
+async def register_service(handlers: list[BaseHandler]):
     """注册服务"""
     
     # 连接到 RabbitMQ
-    print("连接到 RabbitMQ", flush=True)
     await log(type="info", message=f"服务注册中")
     connection = await get_rabbitmq_connection()
-    print("连接到 RabbitMQ", flush=True)
     async with connection:
-        print("获取通道", flush=True)
         channel = await connection.channel()
-        print("获取默认交换机", flush=True)
         exchange = channel.default_exchange
-        print("获取默认交换机", flush=True)
         # 初始化处理器
-        print("初始化处理器", flush=True)
-        handler_instance = handler(exchange)
-        print("处理器初始化完成", flush=True)
-
-        # 自动注册所有处理函数
-        registered_methods = []
-        for method_name in dir(handler_instance):
-            print(f"方法名: {method_name}", flush=True)
-            method = getattr(handler_instance, method_name)
-            # 检查是否是处理器函数
-            if hasattr(method, '_queue_name') and hasattr(method, '_request_model'):
-                # 声明队列
-                registered_methods.append(method)
-                print(f"声明队列: {method._queue_name}", flush=True)
-                queue = await channel.declare_queue(method._queue_name)
-                # 注册消费者
-                await queue.consume(
-                    lambda message: handler_instance.process_message(
-                        message,
-                        request_model=method._request_model,
-                        handler_func=method
-                    )
+        for handler in handlers:
+            handler_instance = handler(exchange)
+            queue = await channel.declare_queue(handler.hanlder_name)
+            # 注册消费者
+            await queue.consume(
+                lambda message: handler_instance.process_message(
+                    message,
+                    request_model=handler.request_model,
+                    handler_func=handler
                 )
-        await log(type="info", message=f"服务注册完成 {str(registered_methods)}")
-        print(f"服务注册完成 {str(registered_methods)}", flush=True)
+            )
+        await log(type="info", message=f"服务注册完成 {str(handlers)}")
         try:
             await asyncio.Future()  # 持续运行
         finally:
