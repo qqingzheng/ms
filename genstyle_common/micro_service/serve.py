478fc9787027ef44a53ba970d2dc966fff85475c
Chestnut <88202804+qqingzheng@users.noreply.github.com>
2025-01-20 17:21:21 +0800

    update

--- genstyle_common/micro_service/serve.py
+++ genstyle_common/micro_service/serve.py
@@ -1,9 +1,9 @@
 import asyncio
-import os
+import os, json
 from .handler import BaseHandler
 from ..conn.rabbitmq import get_rabbitmq_connection
 from ..logger.logger import log
-
+import aio_pika
 async def register_service(handlers: list[BaseHandler]):
     """注册服务"""
     print("服务注册中", flush=True)
@@ -12,10 +12,25 @@ async def register_service(handlers: list[BaseHandler]):
     async with connection:
         channel = await connection.channel()
         exchange = channel.default_exchange
-        # 初始化处理器
+        
+        # 注册handler
         handler_instances = []
         for handler in handlers:
             handler_instance = handler(exchange)
+            
+            # 向api-gateway注册
+            queue_name = f"api_gate_way_registry"
+            queue = await channel.declare_queue(queue_name)
+            await exchange.publish(
+                aio_pika.Message(
+                    body=json.dumps({
+                        "service": os.getenv('SERVICE_NAME'),
+                        "handler": handler.hanlder_name
+                    }).encode()
+                ),
+                routing_key=queue_name
+            )
+            
             queue = await channel.declare_queue(f"{os.getenv('SERVICE_NAME')}_{handler.hanlder_name}")
             # 注册消费者
             await queue.consume(
