2079dc2043d183cc220be172275b7c4d49f7fe27
Chestnut <88202804+qqingzheng@users.noreply.github.com>
2025-02-02 01:37:46 +0800

    update

--- genstyle_common/logger/logger.py
+++ genstyle_common/logger/logger.py
@@ -3,44 +3,46 @@ import json
 import aio_pika
 from ..conn.rabbitmq import get_rabbitmq_connection
 
-async def log(type, message, error_type="", error_file="", traceback=""):
+
+async def log(
+    type, message, error_type="", error_file="", traceback="", service_name=None
+):
     connection = await get_rabbitmq_connection()
     async with connection:
         channel = await connection.channel()
-        service_name = os.getenv("SERVICE_NAME", "unknown")
+        if service_name is None:  # 如果service_name为None，则使用当前服务的名称。
+            service_name = os.getenv("SERVICE_NAME", "unknown")
         if type == "error" or type == "critical":
             await channel.default_exchange.publish(
                 aio_pika.Message(
-                    body=json.dumps({
-                        "service": service_name,
-                        "type": type,
-                        "message": message,
-                        "error_type": error_type,
-                        "error_file": error_file,
-                        "traceback": traceback
-                    }).encode()
+                    body=json.dumps(
+                        {
+                            "service": service_name,
+                            "type": type,
+                            "message": message,
+                            "error_type": error_type,
+                            "error_file": error_file,
+                            "traceback": traceback,
+                        }
+                    ).encode()
                 ),
-                routing_key="monitor_log"
+                routing_key="monitor_log",
             )
         elif type == "warning":
             await channel.default_exchange.publish(
                 aio_pika.Message(
-                    body=json.dumps({
-                        "service": service_name,
-                        "type": type,
-                        "message": message
-                    }).encode()
+                    body=json.dumps(
+                        {"service": service_name, "type": type, "message": message}
+                    ).encode()
                 ),
-                routing_key="monitor_log"
+                routing_key="monitor_log",
             )
         else:
             await channel.default_exchange.publish(
                 aio_pika.Message(
-                    body=json.dumps({
-                        "service": service_name,
-                        "type": type,
-                        "message": message
-                    }).encode()
+                    body=json.dumps(
+                        {"service": service_name, "type": type, "message": message}
+                    ).encode()
                 ),
-                routing_key="monitor_log"
+                routing_key="monitor_log",
             )
