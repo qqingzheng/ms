edc8212c85ea3746f36fd5439b329acf6313e23c
Chestnut <88202804+qqingzheng@users.noreply.github.com>
2025-04-09 15:29:57 +0800

    update

--- genstyle_common/micro_service/inner_request.py
+++ genstyle_common/micro_service/inner_request.py
@@ -4,50 +4,38 @@ import aio_pika
 import time
 from ..conn.rabbitmq import get_rabbitmq_connection
 
-async def inner_request(service_name: str, queue_name: str, request: dict):
-    retry_attempts = 3
+async def inner_request(service_name: str, queue_name: str, request: dict, timeout: int = 3):
     request["inner_request"] = True
-    for attempt in range(retry_attempts):
-        try:
-            connection = await get_rabbitmq_connection()
-            async with connection:
-                channel = await connection.channel()
-
-                # 声明请求队列
-                request_queue = f"{service_name}_{queue_name}"
-
-                # 声明回调队列
-                callback_queue = await channel.declare_queue(exclusive=True)
-
-                # 存储correlation_id和结果的变量
-                correlation_id = str(hash(f"{service_name}_{queue_name}_{time.time()}"))
-                future = asyncio.Future()
-
-                # 定义回调函数处理返回结果
-                async def on_response(message: aio_pika.IncomingMessage):
-                    if message.correlation_id == correlation_id:
-                        future.set_result(json.loads(message.body))
-
-                # 监听回调队列
-                await callback_queue.consume(on_response)
-
-                # 发送消息到对应服务的队列
-                await channel.default_exchange.publish(
-                    aio_pika.Message(
-                        body=json.dumps(request).encode(),
-                        correlation_id=correlation_id,
-                        reply_to=callback_queue.name,
-                    ),
-                    routing_key=request_queue,
-                )
-
-                # 等待回复
-                response = await future
-                return response
-
-        except Exception as e:
-            if attempt < retry_attempts - 1:
-                time.sleep(0.5)
-                print(f"内部接口访问错误，重试次数: {attempt + 1}，错误信息: {str(e)}", flush=True)
-            else:
-                raise Exception("Internal Server Error")
+    try:
+        connection = await get_rabbitmq_connection()
+        async with connection:
+            channel = await connection.channel()
+            # 声明请求队列
+            request_queue = f"{service_name}_{queue_name}"
+            # 声明回调队列
+            callback_queue = await channel.declare_queue(exclusive=True)
+            # 存储correlation_id和结果的变量
+            correlation_id = str(hash(f"{service_name}_{queue_name}_{time.time()}"))
+            future = asyncio.Future()
+            # 定义回调函数处理返回结果
+            async def on_response(message: aio_pika.IncomingMessage):
+                if message.correlation_id == correlation_id:
+                    future.set_result(json.loads(message.body))
+            # 监听回调队列
+            await callback_queue.consume(on_response)
+            # 发送消息到对应服务的队列
+            await channel.default_exchange.publish(
+                aio_pika.Message(
+                    body=json.dumps(request).encode(),
+                    correlation_id=correlation_id,
+                    reply_to=callback_queue.name,
+                ),
+                routing_key=request_queue,
+            )
+            # 等待回复
+            print(f"内部请求「{service_name}」 {queue_name} 等待回复，超时时间：{timeout}", flush=True)
+            response = await asyncio.wait_for(future, timeout=timeout)
+            print(f"内部请求「{service_name}」 {queue_name} 回复成功", flush=True)
+            return response
+    except Exception as e:
+        raise Exception("Internal Server Error")
--- genstyle_common/micro_service/serve.py
+++ genstyle_common/micro_service/serve.py
@@ -54,45 +54,45 @@ async def register_service(handlers: list[BaseHandler]):
     except Exception as e:
         await log("critical", f"服务注册失败: {str(e)}", traceback=traceback.format_exc())
             
-async def inner_request(service_name: str, queue_name: str, request: dict):
-    """内部请求处理 - 同步等待微服务返回"""
-    connection = await get_rabbitmq_connection()
-    request["inner_request"] = True
-    try:
-        async with connection:
-            channel = await connection.channel()
+# async def inner_request(service_name: str, queue_name: str, request: dict):
+#     """内部请求处理 - 同步等待微服务返回"""
+#     connection = await get_rabbitmq_connection()
+#     request["inner_request"] = True
+#     try:
+#         async with connection:
+#             channel = await connection.channel()
 
-            # 声明请求队列
-            request_queue = f"{service_name}_{queue_name}"
+#             # 声明请求队列
+#             request_queue = f"{service_name}_{queue_name}"
 
-            # 声明回调队列
-            callback_queue = await channel.declare_queue(exclusive=True)
+#             # 声明回调队列
+#             callback_queue = await channel.declare_queue(exclusive=True)
 
-            # 存储correlation_id和结果的变量
-            correlation_id = str(hash(f"{service_name}_{queue_name}_{time.time()}"))
-            future = asyncio.Future()
+#             # 存储correlation_id和结果的变量
+#             correlation_id = str(hash(f"{service_name}_{queue_name}_{time.time()}"))
+#             future = asyncio.Future()
 
-            # 定义回调函数处理返回结果
-            async def on_response(message: aio_pika.IncomingMessage):
-                if message.correlation_id == correlation_id:
-                    future.set_result(json.loads(message.body))
+#             # 定义回调函数处理返回结果
+#             async def on_response(message: aio_pika.IncomingMessage):
+#                 if message.correlation_id == correlation_id:
+#                     future.set_result(json.loads(message.body))
 
-            # 监听回调队列
-            await callback_queue.consume(on_response)
+#             # 监听回调队列
+#             await callback_queue.consume(on_response)
 
-            # 发送消息到对应服务的队列
-            await channel.default_exchange.publish(
-                aio_pika.Message(
-                    body=json.dumps(request).encode(),
-                    correlation_id=correlation_id,
-                    reply_to=callback_queue.name,
-                ),
-                routing_key=request_queue,
-            )
+#             # 发送消息到对应服务的队列
+#             await channel.default_exchange.publish(
+#                 aio_pika.Message(
+#                     body=json.dumps(request).encode(),
+#                     correlation_id=correlation_id,
+#                     reply_to=callback_queue.name,
+#                 ),
+#                 routing_key=request_queue,
+#             )
 
-            # 等待回复
-            response = await future
-            return response
+#             # 等待回复
+#             response = await future
+#             return response
 
-    except Exception as e:
-        raise Exception("Internal Server Error")
+#     except Exception as e:
+#         raise Exception("Internal Server Error")
