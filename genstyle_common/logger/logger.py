e42f1eb44dce9e93616e5785c1f8c64e71551209
Chestnut <88202804+qqingzheng@users.noreply.github.com>
2025-04-09 15:54:03 +0800

    update

--- genstyle_common/logger/logger.py
+++ genstyle_common/logger/logger.py
@@ -6,33 +6,36 @@ async def log(
     type, message, error_type="", error_file="", traceback="", user_id=None, request_id=None, service_name=None
 ):
     print(f"正在记录日志，类型：{type}，消息：{message}，错误类型：{error_type}，错误文件：{error_file}，traceback：{traceback}，用户ID：{user_id}，请求ID：{request_id}，服务名称：{service_name}", flush=True)
-    connection = await get_rabbitmq_connection()
-    async with connection:
-        channel = await connection.channel()
-        if service_name is None:  # 如果service_name为None，则使用当前服务的名称。
-            service_name = os.getenv("SERVICE_NAME", "unknown")
-        if type == "error" or type == "critical":
-            response = await inner_request("monitor", "log", {
-                "service": service_name,
-                "type": type,
-                "message": message,
-                "error_type": error_type,
-                "error_file": error_file,
-                "traceback": traceback,
-                "user_id": user_id,
-                "request_id": request_id,
-            }, timeout=3)
-        elif type == "warning":
-            response = await inner_request("monitor", "log", {
-                "service": service_name,
-                "type": type,
-                "message": message,
-                "user_id": user_id,
-                "request_id": request_id,
-            }, timeout=3)
-        else:
-            response = await inner_request("monitor", "log", {
-                "service": service_name,
-                "type": type,
-                "message": message,
-            }, timeout=3)
+    try:
+        connection = await get_rabbitmq_connection()
+        async with connection:
+            channel = await connection.channel()
+            if service_name is None:  # 如果service_name为None，则使用当前服务的名称。
+                service_name = os.getenv("SERVICE_NAME", "unknown")
+            if type == "error" or type == "critical":
+                response = await inner_request("monitor", "log", {
+                    "service": service_name,
+                    "type": type,
+                    "message": message,
+                    "error_type": error_type,
+                    "error_file": error_file,
+                    "traceback": traceback,
+                    "user_id": user_id,
+                    "request_id": request_id,
+                }, timeout=3)
+            elif type == "warning":
+                response = await inner_request("monitor", "log", {
+                    "service": service_name,
+                    "type": type,
+                    "message": message,
+                    "user_id": user_id,
+                    "request_id": request_id,
+                }, timeout=3)
+            else:
+                response = await inner_request("monitor", "log", {
+                    "service": service_name,
+                    "type": type,
+                    "message": message,
+                }, timeout=3)
+    except Exception as e:
+        print(f"记录日志失败：{e}", flush=True)
